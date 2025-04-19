f79d55bb231ea75ad16ebc9316b49bfff3128158
liht0 <liht0yager@gmail.com>
2025-04-19 10:45:28 +1000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "rascheti.h"
+
+int main() {
+    double a, b, c;
+
+    printf("vvedite dlinu storon a, b and c: ");
+    if (scanf("%lf %lf %lf", &a, &b, &c) != 3) {
+        printf("Chisla pishi.\n");
+        return 1;
+    }
+
+    if (!proverka(a, b, c)) {
+        printf("Takogo treugolnika ne sushestvuet.\n");
+        return 2;
+    }
+
+    printf("Perimetr: %.2f\n", perimetr(a, b, c));
+    printf("Ploshad: %.2f\n", ploshad(a, b, c));
+
+    return 0;
+}
